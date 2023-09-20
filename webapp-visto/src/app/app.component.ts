cc7e36c45ead8b3af8d8f48abbfe0ca630913d55
Shander <s.hander8910@hotmail.com>
2023-09-20 11:02:02 -0300

    Criado função para iniciar chamada de init

--- webapp-visto/src/app/app.component.ts
+++ webapp-visto/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, switchMap, takeUntil } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+
+import { FilmeService } from './filme.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'webapp-visto';
+export class AppComponent  {
+  searchControl: FormControl = new FormControl();
+  filmes$: Observable<any[]> = new Observable();
+  private readonly unsubscribe$ = new Subject<void>();
+
+  constructor(private filmeService: FilmeService) {}
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  ngOnInit() {
+    this.filmes$ = this.searchControl.valueChanges
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        switchMap(
+          searchString => this.filmeService.fetchFilmePorNome(searchString)
+        ),
+        map((res:any) => res.Search)
+      );
+
+  }
+
 }
