dc3718eb5d75e10ea37dbcc15a9cdc691e23c00d
Shander <s.hander8910@hotmail.com>
2023-09-20 14:39:54 -0300

    Adicionado tipagem

--- webapp-visto/src/app/filme.service.ts
+++ webapp-visto/src/app/filme.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { FilmeResponse } from './models/filmeResponse';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,11 @@ export class FilmeService {
 
   constructor(private http: HttpClient) { }
 
-  fetchFilmePorNome(query: string) {
-    return this.http.get(`https://www.omdbapi.com/?apikey=${environment.apiKey}&s=${query}`);
+  fetchListaFilmesPorNome(nomeFilme: string) {
+    return this.http.get(`https://www.omdbapi.com/?apikey=${environment.apiKey}&s=${nomeFilme}`);
+  }
+
+  fetchFilmePorId(filmeId: string): Observable<FilmeResponse>{
+    return this.http.get<FilmeResponse>(`https://www.omdbapi.com/?apikey=${environment.apiKey}&i=${filmeId}`);
   }
 }
