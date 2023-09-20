33ad5396b295de7d90a7d53e0722b1fad597abb5
Shander <s.hander8910@hotmail.com>
2023-09-20 11:00:15 -0300

    Adicionado dependências para a service

--- webapp-visto/src/app/app.module.ts
+++ webapp-visto/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FilmeService } from './filme.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [FilmeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
