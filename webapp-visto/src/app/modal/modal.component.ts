ae594d29a6116ddcb4aead0cc407e0183865c0bd
Shander <s.hander8910@hotmail.com>
2023-09-20 16:53:39 -0300

    Criacao componente de modal

--- webapp-visto/src/app/modal/modal.component.ts
+++ webapp-visto/src/app/modal/modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FilmeResponse } from '../models/filmeResponse';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<ModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: FilmeResponse,
+  ) { }
 
   ngOnInit(): void {
+    console.log(this.data)
+  }
+
+  fechar() {
+    this.dialogRef.close();
   }
 
 }
